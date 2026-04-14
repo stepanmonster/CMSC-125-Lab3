5fd2486d861da1d8e03dfa51b43b92ea79b29751
stepanmonster <sgniedes@up.edu.ph>
2026-04-14 23:32:16 +0800

    feat: initial implementation of input parsing

---
+++ bankdb/include/transaction.h
@@ -0,0 +1,37 @@
+#include <pthread.h>
+
+typedef enum {
+    OP_DEPOSIT,   // Add money to account
+    OP_WITHDRAW,  // Remove money from account
+    OP_TRANSFER,  // Move money between two accounts
+    OP_BALANCE,   // Read account balance
+} OpType;
+
+typedef struct {
+    OpType type;
+    int account_id;          // Primary account
+    int amount_centavos;     // Amount in centavos
+    int target_account;      // For TRANSFER only
+} Operation;
+
+typedef enum {
+    TX_RUNNING,
+    TX_COMMITTED,
+    TX_ABORTED
+} TxStatus;
+
+typedef struct {
+    int tx_id;
+    Operation ops[256];    // Max 256 operations per transaction
+    int num_ops;
+    int start_tick;       // When transaction should start
+    pthread_t thread;
+    
+    // Timing (measured in ticks)
+    int actual_start;
+    int actual_end;
+    int wait_ticks;
+    
+    // Status
+    TxStatus status;
+} Transaction;
